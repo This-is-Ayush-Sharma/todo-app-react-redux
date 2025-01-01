7dc75ebac069b4e424e776b2e69ccec26df5dcf1
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2025-01-01 13:17:51 +0530

    code splitting done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import React, { lazy, Suspense } from "react";
 import "./App.css";
-import AddTask from "./components/AddTask";
+// import AddTask from "./components/AddTask";
 import Container from "./components/Container";
 import Header from "./components/Header";
-import TaskList from "./components/TaskList";
+// import TaskList from "./components/TaskList";
+
+const AddTask = lazy(() => import("./components/AddTask"));
+const TaskList = lazy(() => import("./components/TaskList"));
 
 function App() {
   return (
     <>
       <Container>
         <Header />
-        <AddTask />
-        <TaskList />
+        <Suspense fallback={<div>Loading...</div>}>
+          <AddTask />
+          <TaskList />
+        </Suspense>
       </Container>
     </>
   );
