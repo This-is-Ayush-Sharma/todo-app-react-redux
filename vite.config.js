9196e7066244dee80f39411f47db4ac9763df6ae
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2025-01-01 13:22:41 +0530

    not serving legacy JavaScript to modern browsers

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // Ensure modern JavaScript is used
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        // Ensure that the output is optimized for modern browsers
+        format: 'es',
+      },
+    },
+  },
 })
